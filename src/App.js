1e8f36bb047e1b7dbb5a7b8e126cd0777feda805
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-07-02 16:48:14 +0530

    update 'step' field to range field and give reset button

--- src/App.js
+++ src/App.js
@@ -9,33 +9,43 @@ export default function App() {
 }
 
 function Counter() {
+  function resetValues() {
+    setStep(1);
+    setCount(0);
+  }
+
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
+
   const date = new Date();
   date.setDate(date.getDate() + count);
-
-  function incrementCount() {
-    setCount(count + step);
-  }
-
-  function decrementCount() {
-    setCount(count - step);
-  }
 
   return (
     <>
       {/* step */}
-      <div className="">
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
-        Step: {step}
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <label>Step: {step}</label>
       </div>
+
       {/* count */}
-      <div className="">
-        <button onClick={decrementCount}>-</button>
-        Count: {count}
-        <button onClick={incrementCount}>+</button>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          id="count"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
+
       {/* output */}
       <span>
         {count === 0
@@ -45,6 +55,13 @@ function Counter() {
           : `${Math.abs(count)} days ago was `}
       </span>
       <span>{date.toDateString()}</span>
+
+      {/* Reset button */}
+      <div>
+        {(count !== 0 || step !== 1) && (
+          <button onClick={resetValues}>Reset</button>
+        )}
+      </div>
     </>
   );
 }
